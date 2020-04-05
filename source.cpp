1a0d58ce4f2c13165511c2668bf05d5170d6590d
twentysix <54219970+26twentysix@users.noreply.github.com>
2020-04-06 02:10:13 +0500

    добавление препятствия и небольшие стилистические доработки

--- source.cpp
+++ source.cpp
@@ -9,19 +9,40 @@ int main() {
 	const double PI = 3.14159265358979323846;
 	double x = 0;
 	double y = 0;
-	double angleInRad = 0;
-	double time = 0;
-	double timeInFly = 0;
-	double angle = 0;
-	double speed = 0;
-	cout << "Введите скорость тела(в м/c), угол(в градусах) броска относительно земли и время(в секундах), прошедшее с момента броска: \n";
-	cin >> speed >> angle >> time;
-	angleInRad = angle * (PI / 180);
-	timeInFly = (2 * speed * sin(angleInRad)) / G; 
-	if (timeInFly < time) { 
-		time = timeInFly;
+	double angleInRad;
+	double time;
+	double timeInFly;
+	double angle;
+	double speed;
+	double collisionSpeed;
+	bool trigger = true;
+	double arrayOfX[] = { 10, 11 };
+	double arrayOfY[] = { 10, 11 };
+	cout << "Введите скорость тела(в м/c) и угол(в градусах) броска относительно земли \n";
+	cin >> speed >> angle;
+	angleInRad = angle * (PI / 180); 
+	time = (2 * speed * sin(angleInRad)) / G;
+	double strtTime = 0.00001;
+	while (trigger == true) {
+		y = speed * sin(angleInRad) * strtTime - ((G * (strtTime * strtTime)) / 2); 
+		x = speed * cos(angleInRad) * strtTime; 
+		collisionSpeed = sqrt(pow(speed, 2) * pow(sin(angleInRad), 2) + pow((speed * sin(angleInRad) - G * pow(strtTime, 2)), 2));
+		if (x > arrayOfX[0] && x < arrayOfX[1]) {
+			if (y > arrayOfY[0] && y < arrayOfY[1]) {
+				trigger = false;
+				cout << "Произошло столкновение на скорости " << collisionSpeed << ",координаты тела в момент столкновения: " << x << ", " << y << "\n";
+				if (x < arrayOfX[1]) {
+					cout << "После столкновения тело упало вниз до координаты y = 0";
+				}
+			}
+		}
+		if (strtTime > time) {
+			strtTime = time;
+			y = speed * sin(angleInRad) * strtTime - ((G * (strtTime * strtTime)) / 2);
+			x = speed * cos(angleInRad) * strtTime;
+			trigger = false;
+			cout << "Столкновения не произошло, координаты тела: " << x << ", " << y;
+		}
+		strtTime += 0.00001;
 	}
-	y = (speed * sin(angleInRad) * time - ((G * (time * time)) / 2)); 
-	x = speed * cos(angleInRad) * time; 
-	cout << "Координаты тела в заданный момент времени: " << x << ", " << y; 
 }
